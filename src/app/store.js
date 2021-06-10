426c8a5232e62be4742756aeedccfe7665355724
Nishant Kumar Tiwari <nishant99tiwari@gmail.com>
2021-06-09 21:55:54 -0700

    Add files via upload

---
+++ src/app/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from '@reduxjs/toolkit';
+import movieReducer from "../features/movie/movieSlice";
+import userSlice from '../features/user/userSlice';
+export const store = configureStore({
+  reducer: {
+    movie: movieReducer,
+    user: userSlice,
+  },
+});
